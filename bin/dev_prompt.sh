e47f0d722b614601e406d3b75918f5c7bf6e9e1c
Gabriel Fernandez <gabriel.fernandez@foss.st.com>
2023-10-31 09:29:39 +0100

    super continue

--- bin/dev_prompt.sh
+++ bin/dev_prompt.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-gdebug_prompt=""
-if [ "$gdebug" == "true" ]; then
-	gdebug_prompt="(debug) "
+default_prompt()
+{
+	gdebug_prompt=""
+	if [ "$gdebug" == "true" ]; then
+		gdebug_prompt="(debug) "
+	fi
+
+	ps1_login_and_machin='\[\033[00;35m\]\u@\h:'
+	ps1_path='\[\033[00;36m\]\w'
+	ps1_branch='\[\033[1;35m\]$(__git_ps1)\n'
+	# ps1_gprompt='\W'_$1
+	ps1_gprompt=$1/'\W'
+	ps1_newline="\[\033[0;35m\]└─ \[\033[1;34m\] $gdebug_prompt$ps1_gprompt \[\033[0;35m\]\$ \[\033[0m\]"
+
+	export PS1="$ps1_login_and_machin $ps1_path $ps1_branch $ps1_newline"
+}
+
+prompt=`g_config get_dev_prompt`
+
+if test "$prompt"; then
+	if [ "$(type -t $prompt)" = "function" ]; then
+		eval $prompt $1
+	else
+		echo "$prompt doesn't existe !"
+	fi
 fi
-
-ps1_login_and_machin='\[\033[00;35m\]\u@\h:'
-ps1_path='\[\033[00;36m\]\w'
-ps1_branch='\[\033[1;35m\]$(__git_ps1)\n'
-# ps1_gprompt='\W'_$1
-ps1_gprompt=$1/'\W'
-ps1_newline="\[\033[0;35m\]└─ \[\033[1;34m\] $gdebug_prompt$ps1_gprompt \[\033[0;35m\]\$ \[\033[0m\]"
-
-export PS1="$ps1_login_and_machin $ps1_path $ps1_branch $ps1_newline"
